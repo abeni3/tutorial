a865f1d6d9091515a7340100e9196df1f017af83
abeni3 <abenitamene2023@u.northwestern.edu>
2022-10-07 18:25:51 -0500

    schedule popup

--- scheduler/src/components/CourseList.js
+++ scheduler/src/components/CourseList.js
@@ -1,67 +1,100 @@
-import React, { useState } from 'react';
-import { terms, getCourseTerm  } from '../utilities/times.js';
-import { signInWithGoogle, signOut, useUserState } from '../utilities/firebase.js';
+import React, { useState } from 'react'
+import { terms, getCourseTerm } from '../utilities/times.js'
+import {
+  signInWithGoogle,
+  signOut,
+  useUserState,
+} from '../utilities/firebase.js'
 
 import Course from './Course'
+import Modal from './Modal'
 
 const CourseList = ({ courses }) => {
-    const [term, setTerm] = useState('Fall');
-    const [selected, setSelected] = useState([]);
-  
-    //returns array of enumerable properties of an object
-    const termCourses = Object.values(courses).filter(course => term === getCourseTerm(course));
-  
-    return (
+  const [term, setTerm] = useState('Fall')
+  const [selected, setSelected] = useState([])
+
+  const termCourses = Object.values(courses).filter(
+    (course) => term === getCourseTerm(course),
+  )
+
+  const [show, setShow] = useState(false)
+  function closeModal() {
+    setShow(false)
+  }
+  return (
     <>
-      <TermSelector term={term} setTerm={setTerm}/>
+      <TermSelector term={term} setTerm={setTerm} selected={selected} />
+      <Modal children={selected} open={show} close={closeModal}></Modal>
+
+      <button
+        className="btn btn-secondary btn-sm"
+        onClick={() => setShow(true)}
+      >
+        Show Courses
+      </button>
+
       <div className="course-list">
-      {termCourses.map(course => <Course key={course.id} course={ course }  selected={selected} setSelected={ setSelected }  />) }
+        {termCourses.map((course) => (
+          <Course
+            key={course.id}
+            course={course}
+            selected={selected}
+            setSelected={setSelected}
+          />
+        ))}
       </div>
     </>
-    );
-  };
+  )
+}
 
-  
-  const SignInButton = () => (
-    <button className="btn btn-secondary btn-sm"
-        onClick={() => signInWithGoogle()}>
-      Sign In
-    </button>
-  );
+const SignInButton = () => (
+  <button
+    className="btn btn-secondary btn-sm"
+    onClick={() => signInWithGoogle()}
+  >
+    Sign In
+  </button>
+)
 
-  const SignOutButton = () => (
-    <button className="btn btn-secondary btn-sm"
-        onClick={() => signOut()}>
-      Sign Out
-    </button>
-  );
-  
+const SignOutButton = () => (
+  <button className="btn btn-secondary btn-sm" onClick={() => signOut()}>
+    Sign Out
+  </button>
+)
 
-
-const TermSelector = ({term, setTerm}) => {
-  const user = useUserState();
+const TermSelector = ({ term, setTerm }) => {
+  const user = useUserState()
   return (
     <div className="btn-toolbar justify-content-between">
       <div className="btn-group">
-      { 
-        Object.values(terms).map(
-          value => <TermButton key={value} term={value} setTerm={setTerm} checked={value === term} />
-        )
-      }
+        {Object.values(terms).map((value) => (
+          <TermButton
+            key={value}
+            term={value}
+            setTerm={setTerm}
+            checked={value === term}
+          />
+        ))}
       </div>
-      { user ? <SignOutButton /> : <SignInButton /> }
+      {user ? <SignOutButton /> : <SignInButton />}
     </div>
-  );
-};
-  
-  const TermButton = ({term, setTerm, checked}) => (
-    <>
-      <input type="radio" id={term} className="btn-check" onChange={() => setTerm(term)} autoComplete="off" checked={checked}/>
-      <label className="btn btn-success m-1 p-2" htmlFor={term}>
-      { term }
-      </label>
-    </>
-  );
+  )
+}
 
+const TermButton = ({ term, setTerm, checked }) => (
+  <>
+    <input
+      type="radio"
+      id={term}
+      className="btn-check"
+      onChange={() => setTerm(term)}
+      autoComplete="off"
+      checked={checked}
+    />
+    <label className="btn btn-success m-1 p-2" htmlFor={term}>
+      {term}
+    </label>
+  </>
+)
 
-  export default CourseList
+export default CourseList
---
+++ scheduler/src/components/Modal.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import Course from './Course'
+import './Modal.css'
+
+// https://codebuckets.com/2021/08/08/bootstrap-modal-dialog-in-react-without-jquery/
+
+const Modal = ({ children, open, close }) => (
+  <div
+    className={`modal ${open ? 'modal-show' : ''}`}
+    tabIndex="-1"
+    role="dialog"
+    onClick={(evt) => {
+      if (evt.target === evt.currentTarget) close()
+    }}
+  >
+    <div className="modal-dialog" role="document">
+      <div className="modal-content">
+        <div className="modal-header">
+          <button
+            type="button"
+            className="btn-close"
+            aria-label="Close"
+            onClick={close}
+          />
+        </div>
+        <div className="modal-body">
+          {children.length === 0
+            ? 'there are no selected classes'
+            : children.map((course) => (
+                <div key={course.id}className="card m-1 p-2">
+
+                    <div className='card-text'>{course.id}</div>
+                    <div className='card-text'>{course.meets}</div>
+                    <div className='card-text'>{course.title}</div>
+
+                </div>)
+              )}
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+export default Modal
